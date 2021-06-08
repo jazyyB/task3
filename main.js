e90970e3a239729eee8208287a80ab4d37191e21
jazyyB <85590258+jazyyB@users.noreply.github.com>
2021-06-09 01:52:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// const numberButtons = document.querySelectorAll('[data-number]');
+// const operationButtons = document.querySelectorAll('[data-operation]');
+// const equalsButton = document.querySelector('[data-equals');
+// const deleteButton = document.querySelector('[data-delete');
+// const allClearButton = document.querySelector('[data-all-clear');
+// const prevoiusOperandTextElement = document.querySelector('[data-previous-operand');
+// const currentOperandTextElement = document.querySelector('[data-current-operand');
+
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+}
+
